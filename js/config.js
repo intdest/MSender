f7733a7a1d320b8412514d10708edc006396a64d
INTDESTCOIN <info@intdestcoin.com>
2022-11-03 00:21:15 +0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,19 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 1.change this address to your own address
+// 2.set how much fee you will recieve,for example:0.01 
+// 3.client total fee= contract small fee + your setting fee
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var FeeReciever = "0xbEBd96aa4e77831De00537576595dAee6F317dAf";
+
+
+var BinanceFee=0.005;
+var PolygonFee=0.01;
+var FantomFee=0.1;
+var RopstenFee=0.00001;
+var HuobiFee=0.01;
+var AvalancheFee=0.1;
+var EthereumFee=0.01;
+var CronosFee=3;
+var PhoenixFee=0.1;
+var CandleFee=1;
